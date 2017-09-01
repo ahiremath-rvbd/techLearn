8e33f80020f5a3a299cf4e17163c8e3b7a107c7f
Ananda Hiremath <ahiremath@ahiremath-mbpro.local>
2017-09-01 15:31:39 +0530

    add

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+import socket
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "You are running server using docker container, WELCOME TO SDP"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
